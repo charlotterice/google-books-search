eb09d45c81bf80c596ace181703296cd704868cc
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-07-07 20:21:20 -0400

    added mongoose connection in server.js created componoent and pages folders

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const path = require("path");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require ("axios");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+mongoose.connect(MONGODB_URI,{useNewUrlParser:true});
 
 // Define API routes here
 
